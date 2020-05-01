658d2187333632692fc8e3d873747dc2c1bdb9a8
Denis <2001d2010t@gmail.com>
2020-05-01 20:22:55 +0300

    Задание 4

---
+++ Задания/script.js
@@ -0,0 +1,85 @@
+let money, time;
+
+function start() { 
+  money = +prompt('Ваш бюджет на месяц?', '');
+  time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+  while(isNaN(money) || money == '' || money == null) {
+    money = +prompt('Ваш бюджет на месяц?', '');
+  }
+}
+
+start();
+
+let 
+  appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
+  };
+
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let 
+      a = prompt('Введите обязательную статью расходов в этом месяце'),
+      b = prompt('Во сколько обойдется?');
+  
+    if ((typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b) != null) && a != '' && b != '' && a.length < 50) {
+      console.log('done');
+      appData.expenses[a] = b;
+    } else {
+      console.log('bad result');
+      i--;
+    }
+  }
+}
+
+chooseExpenses();
+// Расчет дневного бюджета
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+
+  alert('Бюджет на один день составляет: ' + appData.moneyPerDay);
+}
+
+detectDayBudget();
+// Расчет уровня достатка
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+  } else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+  } else {
+    console.log('Что-то пошло не так');
+  }
+}
+
+detectLevel();
+
+function checkSavings() {
+  if (appData.savings == true) {
+    let 
+      save = +prompt('Какова сумма накоплений?'),
+      percent = +prompt('Под какой процент?');
+
+    appData.monthIncome = save / 100 / 12 * percent;
+    alert('Доход в месяц с вашего депозита: ' + (appData.monthIncome).toFixed(1));
+  }
+}
+
+checkSavings();
+// Функция для определения необязательных расходов
+function chooseOptExpenses() {
+  for (let i = 1; i < 3; i++) {
+    let 
+      q = prompt('Введите обязательную статью расходов в этом месяце');
+    appData.optionalExpenses[i] = q; 
+  }
+}
+
+chooseOptExpenses();
