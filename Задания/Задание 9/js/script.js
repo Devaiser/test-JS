0c25778eb678af3caa9b1bdb69e14df584ba6b9a
Denis <2001d2010t@gmail.com>
2020-05-10 18:16:38 +0300

    Задание 10

--- Задания/Задание 9/js/script.js
+++ Задания/Задание 9/js/script.js
@@ -36,4 +36,58 @@ window.addEventListener('DOMContentLoaded', function () {
 			}
 		}
 	});
+
+	//Timer
+
+	let deadline = '2020-05-11';
+
+	function getTimeRemaining(endTime) {
+		let
+			t = Date.parse(endTime) - Date.parse(new Date()),
+			seconds = Math.floor((t / 1000) % 60);
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+		let
+			minutes = Math.floor((t / 1000 / 60) % 60);
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		let
+			hours = Math.floor((t / (1000 * 60 * 60)));
+		if (hours < 10) {
+			hours = '0' + hours;
+		}
+		// hours = Math.floor((t / 1000 / 60 / 60) % 24),
+		// days = Math.floor((t/(1000 * 60 * 60 * 24)));
+
+		return {
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function setClock(id, endTime) {
+		let
+			timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		function updateClock() {
+			let t = getTimeRemaining(endTime);
+			hours.textContent = t.hours;
+			minutes.textContent = t.minutes;
+			seconds.textContent = t.seconds;
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock('timer', deadline);
 });
